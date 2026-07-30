ecdf9465ca0a22923450d851e777c7ef19b929b3
Rohan Chaudhary <rohanchaudharyofficial@gmail.com>
2026-07-30 16:04:11 +0530

    Time: 9803 ms (6.36%) | Memory: 12.6 MB (19.26%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        longest = ""
+
+        for i in range(len(s)):
+            for j in range(i + 1, len(s) + 1):
+                sub = s[i:j]
+                if sub == sub[::-1] and len(sub) > len(longest):
+                    longest = sub
+        
+        return longest
+
+    
+
+    
+        
